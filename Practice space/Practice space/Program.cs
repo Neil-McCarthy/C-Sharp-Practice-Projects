42852de68407f7fd291f21a1e442a8494963b25d
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2022-08-03 15:11:44 +0100

    Adding a project

---
+++ Practice space/Practice space/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice_space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
