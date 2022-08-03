f1192834cbd609985d58b7ccfbf326cac5edae0e
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2022-08-03 19:12:27 +0100

    Enhanced IF  statement

--- Practice space/Practice space/Program.cs
+++ Practice space/Practice space/Program.cs
@@ -7,81 +7,18 @@ namespace HelloWorld
         
         static void Main(string[] args)
         {
-            //BASIC LOGIN SYSTEM
+            //ENHANCED IF STATEMENT
+            Console.Write("Please enter a positive or negative number : ");
+            string userInput = Console.ReadLine();
+            int convertedValue;
+            if (int.TryParse(userInput, out convertedValue))
+            {
+                string result = convertedValue > 0 ? "True" : "False";
+                Console.WriteLine(result);
+            }
 
-            Console.WriteLine("Hello and welcome to the site.\nWould you like to:\n\n1) Sign-up\n\nOr\n\n2) Log-in\n\n");
-            Console.WriteLine("Please enter the number corresponding to the answers above : ");
-            string userAnswer = Console.ReadLine();
-            if (userAnswer == "1")
-            {
-                SignUp();
-            }
-            else if (userAnswer == "2")
-            {
-                Login();
-            }
-            else
-            {
-                Console.WriteLine("Apologies but we were unable to understand your answer.");
-            }
-            Console.Read();
-        }
-
-        public static void SignUp()
-        {
-            string existingUser = "Neil McCarthy";
-            string enteredUsername, enteredPassword;
-            Console.WriteLine("Graet to meet you!\nPlease enter your username here : ");
-            enteredUsername = Console.ReadLine();
-            if (enteredUsername != existingUser)
-            {
-                Console.WriteLine("\nPlease enter your password here : ");
-                enteredPassword = Console.ReadLine();
-                Console.WriteLine("\nPlease re-enter your password here to confirm : ");
-                string enteredPasswordCheck = Console.ReadLine();
-                if (enteredPassword == enteredPasswordCheck)
-                {
-                    Console.WriteLine("Hi " + enteredUsername + "!\nWelcome to the site.\nWe currently offer no services. Goodbye.");
-                }
-                else
-                {
-                    Console.WriteLine("Sorry but it appears those passwords don't match.\nPlease try again.\n\n");
-                    SignUp();
-                }
-            }
-            else
-            {
-                Console.WriteLine("Apologies but it seems like that username is already taken.\nWould you like to try again?\n\ny/n :");
-                string tryAgain = Console.ReadLine();
-                if (tryAgain == "y")
-                {
-                    SignUp();
-                }
-            }
-        }
-
-        public static void Login()
-        {
-            string existingUser = "Neil McCarthy";
-            string existingPassword = "Password01";
-            string enteredUsername, enteredPassword;
-            Console.WriteLine("Please enter your username here : ");
-            enteredUsername = Console.ReadLine();
-            Console.WriteLine("Please enter your password here : ");
-            enteredPassword = Console.ReadLine();
-            if (enteredUsername == existingUser && enteredPassword == existingPassword)
-            {
-                Console.WriteLine("Hi " + enteredUsername + "!\nWelcome back.\nWe currently offer no services. Goodbye.");
-            } else if (enteredUsername != existingUser)
-            {
-                Console.WriteLine("Sorry but we can't recognise that username.\nHave another go.");
-                Login();
-            }
-            else if (enteredPassword != existingPassword)
-            {
-                Console.WriteLine("Sorry but that password does not match this account.\nHave another go.");
-                Login();
-            }
+            
+            Console.ReadLine();
         }
     }
 }
