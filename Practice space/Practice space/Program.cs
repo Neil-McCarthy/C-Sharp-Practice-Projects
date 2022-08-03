dd4a21d32a36ffe752765e51ec4e4fcc32e8779c
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2022-08-03 19:19:28 +0100

    Binary if exercise

--- Practice space/Practice space/Program.cs
+++ Practice space/Practice space/Program.cs
@@ -7,13 +7,13 @@ namespace HelloWorld
         
         static void Main(string[] args)
         {
-            //ENHANCED IF STATEMENT
-            Console.Write("Please enter a positive or negative number : ");
+            //ENHANCED IF EXERCISE
+            Console.Write("Please enter a temperature : ");
             string userInput = Console.ReadLine();
             int convertedValue;
             if (int.TryParse(userInput, out convertedValue))
             {
-                string result = convertedValue > 0 ? "True" : "False";
+                string result = convertedValue <= 15? "it is too cold here" : convertedValue <= 28 ? "it is ok" : convertedValue > 28 ? "it is hot here" : "";
                 Console.WriteLine(result);
             }
 
