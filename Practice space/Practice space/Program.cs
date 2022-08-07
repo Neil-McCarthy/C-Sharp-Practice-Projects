375c589c0e17efdef44e6f36c8ee557675d44bce
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2022-08-07 12:20:22 +0100

    first use of classes

--- Practice space/Practice space/Program.cs
+++ Practice space/Practice space/Program.cs
@@ -1,44 +1,19 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace Practice_space
 {
-    internal class Program
+    class Program
     {
         
         static void Main(string[] args)
         {
 
-            //AVERAGE SCORE EXERCISE
-            float avgScore = 0f;
-            int numberOfInputs = 0; 
-            int totalScore = 0;
-            Console.WriteLine("Hello! Please enter some values between 0 and 20 and we will calculate the average.");
-            Console.WriteLine("Remember to type -1 to stop inputs and get your average.");
-            while (true)
-            {
-                Console.Write("Please enter on of your values here : ");
-                string userInput = Console.ReadLine();
-                int userInputConverted;
-                if (int.TryParse(userInput, out userInputConverted)) {
-                    if (userInputConverted >= 0 && userInputConverted <= 20)
-                    {
-                        totalScore += userInputConverted;
-                        numberOfInputs++;
-                    } else if (userInputConverted == -1) {
-                        avgScore = totalScore / numberOfInputs;
-                        Console.WriteLine("Thank you for using us.\nYour average score comes to " + avgScore);
-                        break;
-                    } else
-                    {
-                        Console.WriteLine("Looks like your input is not between 0 and 20. Please try again.");
-                        //continue;
-                    }
-                } else
-                {
-                    Console.WriteLine("Sorry but we were unable to read your input. Please try again.");
-                    //continue;
-                }
-            }
+            //CLASSES
+
+            Human denis = new Human();
+            denis.firstName = "Denis";
+            denis.lastName = "The Menace";
+            denis.IntroduceMyself();
             Console.ReadLine();
         }
     }
