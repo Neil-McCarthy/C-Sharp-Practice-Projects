58fcd7440369694ba1feea06e2a6894e0113e753
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2022-08-09 12:15:41 +0100

    Array - string

--- Practice space/Practice space/Program.cs
+++ Practice space/Practice space/Program.cs
@@ -15,9 +15,21 @@ namespace Practice_space
             grades[2] = 3;
             grades[3] = 2;
             grades[4] = 1;
+
+            string[] names = new string[5];
+            names[0] = "Bingo";
+            names[1] = "Flingo";
+            names[2] = "Mojo";
+            names[3] = "Gojo";
+            names[4] = "Blinko";
             for (int index = 0;index < grades.Length; index++)
             {
                 Console.WriteLine(grades[index]);
+            }
+            Console.WriteLine("");
+            for (int index = 0; index < names.Length; index++)
+            {
+                Console.WriteLine(names[index]);
             }
 
             Console.ReadLine();
