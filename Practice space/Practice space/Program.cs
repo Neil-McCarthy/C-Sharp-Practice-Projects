5344aaa29f41b59385921dc4dab206274615709a
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2022-08-16 19:42:11 +0100

    Tic Tac Toe - unfinished

--- Practice space/Practice space/Program.cs
+++ Practice space/Practice space/Program.cs
@@ -4,53 +4,84 @@ namespace Practice_space
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
 
-            //MULTI DIMENSIONAL ARRAYS
-
-            //TWO DIMENSIONAL 
-            int[,] arrays2D = new int[,]
+            //TIC TAC TOE
+            string[,] ticTacToe = new string[,]
             {
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
+                {"-","-","-"},
+                {"-","-","-"},
+                {"-","-","-"}
             };
+            
 
-            //THREE DIMENSIONAL
-            int[,,] arrays3D = new int[,,]
+
+            bool gamePlaying = true;
+            bool playerSelector = true;
+            int userInputRow;
+            //int userInputRowConverted;
+            int userInputColumn;
+            //int userInputColumnConverted;
+            string playerOutput;
+            string output;
+            while (gamePlaying)
             {
+                Console.WriteLine("Current board:\n");
+                for (int row = 0; row < ticTacToe.GetLength(0); row++)
                 {
-                    {000, 001},
-                    {010, 011}
-                },
-                {
-                    {100, 101},
-                    {110, 111}
-                }
-            };
-
-
-            //NESTED FOR LOOPS METHOD - PRINT ONLY EVEN NUMBERS
-            for (int index = 0; index < arrays2D.GetLength(0); index++)
-            {
-                for (int blindex = 0; blindex < arrays2D.GetLength(1); blindex++)
-                {
-                    if (arrays2D[index, blindex] % 2 == 0)
+                    output = "";
+                    for (int column = 0; column < ticTacToe.GetLength(1); column++)
                     {
-                        Console.WriteLine(arrays2D[index, blindex]);
+                        output = output + ticTacToe[row, column].ToString() + " ";
+                    }
+                    Console.WriteLine(output);
+                }
+
+                //MUST COME BACK AND DO USER INPUT CHECKS
+                Console.WriteLine("\nPlease enter the row number :");
+                userInputRow = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter the column number :");
+                userInputColumn = Int32.Parse(Console.ReadLine());
+                if (playerSelector == true)
+                {
+                    playerOutput = "X";
+                    playerSelector = false;
+                } else
+                {
+                    playerOutput = "O";
+                    playerSelector = true;
+                }
+                ticTacToe[userInputRow, userInputColumn] = playerOutput;
+
+                //MUST COME BACK AND MAKE THIS A FUNCTION
+                for (int counter = 0; counter < 3; counter++)
+                {
+                    if (ticTacToe[counter, 0] == "X" && ticTacToe[counter, 1] == "X" && ticTacToe[counter, 2] == "X" || ticTacToe[0, counter] == "X" && ticTacToe[1, counter] == "X" && ticTacToe[2, counter] == "X")
+                    {
+                        Console.WriteLine("Player One wins with Xs. Well done!");
+                        gamePlaying = false;
+                    }
+                    if (ticTacToe[counter, 0] == "O" && ticTacToe[counter, 1] == "O" && ticTacToe[counter, 2] == "O" || ticTacToe[0, counter] == "O" && ticTacToe[1, counter] == "O" && ticTacToe[2, counter] == "O")
+                    {
+                        Console.WriteLine("Player Two wins with Os. Well done!");
+                        gamePlaying = false;
                     }
                 }
-            }
-            /*foreach (int index in arrays2D)
-            {
-                Console.Write(index + " ");
-            }*/
+                if (ticTacToe[0, 0] == "X" && ticTacToe[1, 1] == "X" && ticTacToe[2, 2] == "X" || ticTacToe[0, 2] == "X" && ticTacToe[1, 1] == "X" && ticTacToe[2, 0] == "X")
+                {
+                    Console.WriteLine("Player One wins with Xs. Well done!");
+                    gamePlaying = false;
+                }
+                if (ticTacToe[0, 0] == "O" && ticTacToe[1, 1] == "O" && ticTacToe[2, 2] == "O" || ticTacToe[0, 2] == "O" && ticTacToe[1, 1] == "O" && ticTacToe[2, 0] == "O")
+                {
+                    Console.WriteLine("Player Two wins with Os. Well done!");
+                    gamePlaying = false;
+                }
 
+                }
             Console.ReadLine();
 
-            
         }
     }
 }
