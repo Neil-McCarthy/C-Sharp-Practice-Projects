747896ec05c1582f8c0a7f402d57ddd3ce1d7af2
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2022-08-24 13:09:56 +0100

    Dictionary - Example - Employee info

--- Practice space/Practice space/Program.cs
+++ Practice space/Practice space/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-//REQUIRED FOR DECLARING ARRAYLIST
-using System.Collections;
+//REQUIRED FOR DECLARING DICTIONARY
+using System.Collections.Generic;
 
 namespace Practice_space
 {
@@ -9,57 +9,68 @@ namespace Practice_space
         //HASHTABLES
         static void Main(string[] args)
         {
-            //CHALLENGE- TAKE ALL VALUES FROM STUDENT ARRAY AND INSERT INTO HASHTABLE
-            //IF ID IS SAME SKIP STUDENT AND DISPLAY ERROR: "Sorry, a student with the same ID already exists."
+            //DICTIONARIES
 
-            Student[] students = new Student[5];
-            students[0] = new Student(1, "Denis", 88);
-            students[1] = new Student(2, "Olaf", 97);
-            students[2] = new Student(6, "Ragnar", 65);
-            students[3] = new Student(1, "Luise", 73);
-            students[4] = new Student(4, "Levi", 58);
-
-
-            Hashtable studentsTable = new Hashtable();
-
-            foreach (Student studentIndex in students)
+            Dictionary<int, string> myDictionary = new Dictionary<int, string>()
             {
-                if (studentsTable.ContainsKey(studentIndex.Id))
-                {
-                    Console.WriteLine("Sorry, a student with the same ID already exists.");
-                } else
-                {
-                    studentsTable.Add(studentIndex.Id, studentIndex);
-                }
+                { 1, "one" },
+                { 2, "two" },
+                { 3, "three" }
+            };
+
+
+            // DICTIONARY EXAMPLE - EMPLOYEE INFO
+
+            Employee[] employees =
+            {
+                new Employee("CEO","Gwyn",95,200),
+                new Employee("Manager","Joe",35,25),
+                new Employee("HR","Lora",32,21),
+                new Employee("Secretary","Petra",28,18),
+                new Employee("Lead Developer","Artorias",55,35),
+                new Employee("Intern","Ernest",22,8),
+            };
+
+            Dictionary<string, Employee> employeesDictionary = new Dictionary<string, Employee>();
+            foreach (Employee emp in employees)
+            {
+                employeesDictionary.Add(emp.Role, emp);
             }
 
-            foreach (Student value in studentsTable.Values)
-            {
-                Console.WriteLine("Student ID:{0},", value.Id);
-                Console.WriteLine("Student Name:{0},", value.Name);
-                Console.WriteLine("Student GPA:{0},", value.GPA);
-            }
+            Employee emp1 = employeesDictionary["Intern"];
+            Console.WriteLine("Employee Name: {0}, Role: {1}, Salary: {2}, Rate: {3} \n", emp1.Name, emp1.Role, emp1.Salary, emp1.Rate);
+            
             Console.ReadLine();
         }
     }
 
-    class Student
+    class Employee
     {
-        //property called Id
-        public int Id { get; set; }
+        public string Role { get; set; }
 
-        //property called Name
         public string Name { get; set; }
 
-        //property called GPA
-        public float GPA { get; set; }
+        public int Age { get; set; }
 
-        //constructor
-        public Student(int id, string name, float GPA)
+        public float Rate { get; set; }
+
+        //yearly salary = rate/h * number of days * number of weeks * number of months
+        public float Salary
         {
-            this.Id = id;
+            get
+            {
+                return Rate * 8 * 5 * 4 * 12;
+            }
+        }
+
+        public Employee(string role, string name, int age, float rate)
+        {
+            this.Role = role;
             this.Name = name;
-            this.GPA = GPA;
+            this.Age = age;
+            this.Rate = rate;
         }
     }
+
+    
 }
